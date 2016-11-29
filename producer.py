6c57dbb55b099268fcc382e0ebb77a99ba54185c
mwytock0812 <michael.wytock@gmail.com>
2016-11-29 13:26:27 -0500

    Remove comments.

--- producer.py
+++ producer.py
@@ -1,7 +1,7 @@
 from pykafka import KafkaClient
 client = KafkaClient(hosts="127.0.0.1:9092")
 
-print client.topics  # {'test': <weakref at 0x102b7c578; to 'Topic' at 0x102b7ff10>}
+print client.topics
 topic = client.topics['test']
 producer = topic.get_sync_producer()
 
@@ -9,7 +9,7 @@ count = 0
 while True:
     count += 1
     producer.produce('test msg', partition_key='{}'.format(count))
-    if count % 10 ** 5 == 0:  # adjust this or bring lots of RAM ;)
+    if count % 10 ** 5 == 0:
         while True:
             try:
                 msg, exc = producer.get_delivery_report(block=False)
