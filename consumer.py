6aa47d0470110fa254e6c020f9badcd604d24b5b
Cody Ramaker <cramaker@linux.com>
2017-06-21 10:46:08 -0500

    Updated several lines to work with Python 3

--- consumer.py
+++ consumer.py
@@ -1,13 +1,13 @@
 from pykafka import KafkaClient
 client = KafkaClient(hosts="127.0.0.1:9092")
 
-topic = client.topics['test']
+topic = client.topics[b'test']
 
 balanced_consumer = topic.get_balanced_consumer(
-    consumer_group='testgroup',
+    consumer_group=b'testgroup',
     managed=True
 )
 
 for message in balanced_consumer:
     if message is not None:
-        print message.offset, message.value
+        print(message.offset, message.value)
